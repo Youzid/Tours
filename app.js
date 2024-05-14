9ca33fb67235d86371eaca2e65689c6e03e56dbb
youcefbenbouzid <byoucef@sefrone.com>
2024-05-14 18:19:38 +0200

    Sanitazation

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
 const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHnadler = require('./controllers/errorController');
+const hpp = require('hpp');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) Global MIDDLEWARES
+
+//Security http headers
+app.use(helmet());
+
+// development loggin
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requets, please try again in an hour!'
+});
+app.use(`/api`, limiter);
 
-app.use(express.json());
+// body parse, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parametere pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price '
+    ]
+  })
+);
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware  temporary)
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -29,6 +69,6 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use(globalErrorHnadler)
+app.use(globalErrorHnadler);
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (!allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return obj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -11,6 +20,37 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     }
   });
 });
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (res.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates ', 400));
+  }
+  const filteredBody = filterObj(req.body, 'name', 'email ');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  if (!req.user.id) {
+    return next(new AppError('User not found ', 404));
+  }
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: {
+      user: null
+    }
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
